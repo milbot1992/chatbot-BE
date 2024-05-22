db7ab575af03060fe993021c6cf44ea6ad0528bb
Millie Ellis <millieellis@Millies-MacBook-Pro.local>
2024-05-22 11:55:46 +0100

    special terms for fitness added

--- utility2.py
+++ utility2.py
@@ -1,17 +1,12 @@
 import os
 import openai
-import re
 
 from dotenv import load_dotenv, find_dotenv
-from keywords import prompt_allowed
 
 _ = load_dotenv(find_dotenv())
 
 openai.api_key = os.getenv('GPT_API_KEY')
 
-def is_fitness_related(prompt):
-    pattern = re.compile('|'.join(prompt_allowed), re.IGNORECASE)
-    return bool(pattern.search(prompt))
 
 ## Helper function
 def get_completion(prompt, model="gpt-3.5-turbo", temperature=0):
@@ -36,11 +31,9 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
 
 
 def collect_messages(prompt: str, context: list, temperature=0):
-    if not is_fitness_related(prompt):
-        response = "Sorry, I am a fitness chatbot - please ask me a fitness related question."
-    else:
-        context.append({'role': 'user', 'content': f'{prompt}'})
-        response = get_completion_from_messages(context, temperature=temperature)
-        context.append({'role': 'assistant', 'content': f'{response}'})
+    context.append({'role': 'user', 'content': f'{prompt}'})
+    response = get_completion_from_messages(context, temperature=temperature)
+    # response = "HI!!"
+    context.append({'role': 'assistant', 'content': f'{response}'})
 
     return {'context': context}
